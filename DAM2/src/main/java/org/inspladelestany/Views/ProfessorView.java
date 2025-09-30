49d6d5a88b21c39e19bdb421657b84b2333f02c8
Alexxx <115105013+Alexxxtube69@users.noreply.github.com>
2025-09-30 23:28:42 +0200

    metodo para poder elegir una opc del menu y submenu

--- DAM2/src/main/java/org/inspladelestany/Views/ProfessorView.java
+++ DAM2/src/main/java/org/inspladelestany/Views/ProfessorView.java
@@ -7,22 +7,37 @@ import java.util.Scanner;
 
 public class ProfessorView {
     private static final Scanner scanner = new Scanner(System.in);
+    private static  Menu menu;
 
     //Methods
-    public Professor demanarDadesProfessor(){
+    public Integer menuOpc(int opc) {
+        menu.menuPrincipal();
+        System.out.println("Elige una opc: ");
+        opc = Integer.parseInt(scanner.nextLine());
+        return opc;
+    }
+
+    public Integer menuSecuOpc(int opc) {
+        menu.menuOpcions();
+        System.out.println("Elige una opc: ");
+        opc = Integer.parseInt(scanner.nextLine());
+        return opc;
+    }
+
+    public Professor demanarDadesProfessor() {
         System.out.println("Introdueix el teu nom");
         String nom = scanner.nextLine();
 
         System.out.println("Introdueix el cognom");
         String cognom = scanner.nextLine();
 
-        return new Professor(nom,cognom);
+        return new Professor(nom, cognom);
     }
 
-    public void mostrarDetallsProfessor(Professor prof){
+    public void mostrarDetallsProfessor(Professor prof) {
         System.out.println("Detalls del Professsor:");
         System.out.println("id: " + prof.getId());
         System.out.println("Nom: " + prof.getNom());
-        System.out.println("Cognom: "+ prof.getCognom());
+        System.out.println("Cognom: " + prof.getCognom());
     }
 }
