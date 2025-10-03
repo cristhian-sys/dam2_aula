97a3e590998d6aa042238e44fe109c5cac8b9921
cristhian-sys <cristhianjosuemacias@inspladelestany.cat>
2025-10-03 15:46:41 +0200

    prueba

--- DAM2/src/main/java/org/inspladelestany/Controller/ProfessorController.java
+++ DAM2/src/main/java/org/inspladelestany/Controller/ProfessorController.java
@@ -18,5 +18,25 @@ public class ProfessorController {
         
         int option = professorView.menuSecuOpc();
 
+        do {
+            switch (option){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+
+                default:
+            }
+        }
+
+    }
+
+    public void altaProfessor (){
+        // Datos al professor con Professor view
+        //DAO inserta a la base de dades
     }
 }
